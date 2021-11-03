23354f97071bcbed376788d1ddf1d588964b1ad0
Sophia Belenkova <sophiabelenkova@Sophias-MacBook-Pro.local>
2021-11-04 01:51:42 +0300

    message

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // index.js
 // This is the main entry point of our application
+
+sayHello = () => {
+    console.log("Hello, my darling. I was meant to say this, so...GATHER YOUR THANGS AND GO TO CALIFORNIA! SEE YA THERE! DON\'T WANNA HEAR ANYTHING");
+}
+
+sayHello();
